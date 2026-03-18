0d98c7325664edaa9c8442e1936c0e40725b5a69
Jammyboyloy <jammylay07@gmail.com>
2026-03-18 23:37:04 +0700

    Fix db.js path and SSL for Aiven

--- config/db.js
+++ config/db.js
@@ -3,20 +3,21 @@ const fs = require("fs");
 const path = require("path");
 require("dotenv").config();
 
-const caPath = path.join(__dirname, "../ca.pem"); // db.js is in config/
+// Build the path to ca.pem in the root folder
+const caPath = path.join(__dirname, "../ca.pem"); // config/db.js -> root/ca.pem
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
   ssl: {
-    ca: fs.readFileSync(caPath),   // <-- required by Aiven
-    rejectUnauthorized: true,       // enforce SSL
+    ca: fs.readFileSync(caPath),
+    rejectUnauthorized: true,
   },
 });
 
