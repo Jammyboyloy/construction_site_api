5b4ad3c8f6f3df094efbe16e939441d545889bf1
Jammyboyloy <jammylay07@gmail.com>
2026-03-18 23:31:31 +0700

    New

--- config/db.js
+++ config/db.js
@@ -3,21 +3,20 @@ const fs = require("fs");
 const path = require("path");
 require("dotenv").config();
 
-// Build path to ca.pem in root folder
-const caPath = path.join(__dirname, "../ca.pem"); 
+const caPath = path.join(__dirname, "../ca.pem"); // db.js is in config/
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
   database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
   ssl: {
-    ca: fs.readFileSync(caPath),
-    rejectUnauthorized: true,
+    ca: fs.readFileSync(caPath),   // <-- required by Aiven
+    rejectUnauthorized: true,       // enforce SSL
   },
 });
 
